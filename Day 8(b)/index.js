0f3f37ac634cc62d7da4291a55c570e0f9160298
Vibhu Dixit <vibhudixit.2215001940.com>
2024-09-27 13:13:39 +0530

    day8 completed CRUD

---
+++ Day 8(b)/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({extended: true}));//middleware for form data in express
+// app.use(express.json());//middleware for form data in JSON format
+
+let arr = [
+    {
+        id : 0,
+        username : "Vibhu",
+        comment: "lelo podeena ahhh ahhh "
+    },
+    {
+        id : 1,
+        username : "Shivam",
+        comment: "Aha tamatar bade majedaar "
+    },{
+        id : 2,
+        username : "Madhav",
+        comment: "Asla ham bhi rakhte hai pahalwaann "
+    },{
+        id : 3,
+        username : "Vansh",
+        comment: "Tum dono ki maaki u .... "
+    },
+]
+//root
+app.get('/',(req,res)=>{
+    res.send('Saale blog page par ja ');
+});
+//Task-1 Display all the blogs!
+app.get('/blog',(req,res)=>{
+    res.render('index',{arr});
+})
+//Task -2 Create a form to create a blog
+app.get('/blog/new',(req,res)=>{
+    res.render('new');//jab aap render karte ho toh . na dena padta kyunki usme path default pata hai
+});
+
+//Task -3 actually add blog in db
+app.post('/blog',(req,res)=>{
+    let {username,comment} = req.body;
+    let newBlog = {
+        id : arr.length+1,
+        username,
+        comment
+    }
+    arr.push(newBlog);
+    res.redirect('/blog');
+});
+
+//Task -4 Show a single blog
+app.get('/blog/:id',(req,res)=>{
+    let {id} = req.params;
+    let blog = arr.find(b=>b.id == id);
+    if(!blog){
+        return res.send('Blog not found');
+    }
+    res.render('show',{blog});
+});
+
+app.listen(8000,()=>{
+    console.log('listening on 8000');
+});
--- Day 8/index.js
+++ Day 8/index.js
@@ -5,8 +5,8 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname,'public')));
-// app.use(express.urlencoded({extended: true}));//middleware for form data in express
-app.use(express.json());//middleware for form data in JSON format
+app.use(express.urlencoded({extended: true}));//middleware for form data in express
+// app.use(express.json());//middleware for form data in JSON format
 
 app.get('/',(req,res)=>{
     res.render('index');
